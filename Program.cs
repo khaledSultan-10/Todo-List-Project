200dcc9dd5c5668afaebe6d8d6243f8828ed7439
khaled <khaled911507@gamil.com>
2026-06-27 11:53:43 +0300

    add txt file

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics.Arm;
-
+using System.IO;
 List <string> todolist = new List<string>();
 int userchoose = 0;
+
+
+string fileName = Path.Combine(AppContext.BaseDirectory, "../../../taskmanager.txt");
+// to make sure the file exists before reading it
+if (File.Exists(fileName))
+{
+    string[] lines = File.ReadAllLines(fileName);
+    todolist.AddRange(lines);
+}
 
 System.Console.WriteLine("\"choose what you want to do\"");
 
@@ -24,10 +33,12 @@ else if (  userchoose > 4 || userchoose < 1)
 
 
 // now only numpers between 1 and 4 here
+
 if ( userchoose == 1)
     {
         System.Console.WriteLine("enter your task:");
         todolist.Add(Console.ReadLine());
+        File.WriteAllLines(fileName, todolist);
     }
 
 if (userchoose == 2)
@@ -83,9 +94,11 @@ if (userchoose == 3)
         }
 
         todolist.RemoveAt(removetask - 1);
+        File.WriteAllLines(fileName, todolist);
         break;
 
-    } while (removetask > todolist.Count || removetask < 1 );
+    } while (true);
+
 
 }
 
